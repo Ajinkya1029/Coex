86f66e367da82e6be073dc93ba5b8a4f61626759
Ajinkya <anamra1029@gmail.com>
2023-12-09 20:16:59 +0530

    logincarocomplte

--- client/src/components/body.jsx
+++ client/src/components/body.jsx
@@ -5,13 +5,14 @@ import Meet from "./meet";
 import Contact from "./contact";
 import bgimg from "../resources/backgroundimage.png";
 import style from './style.module.css'
+import carodata from "./carodata";
 
 function Body(){
 return <div >
     <img src={bgimg} className={style.bgimg} ></img>
 <NavBar ></NavBar>
 <Work></Work>
-<Meet></Meet>
+<Meet data={carodata}></Meet>
 <Contact></Contact>
 </div> 
 

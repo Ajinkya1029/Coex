edb66772a719d7837c79eb8cb2e5d94eb010f8cb
Ajinkya <anamra1029@gmail.com>
2023-12-19 00:45:18 +0530

    final

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,3 +1,30 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import CoexLogo from "../resources/coex-logo (1).png";
+import styles from "./style.module.css";
+
+function NavBar() {
+  return (
+    <div className={styles.NavbarContainer}>
+      <div className={styles.LeftNav}>
+        <img id="coexlogo" src={CoexLogo} alt="Logo" />
+      </div>
+
+      <div className={styles.RightNav}>
+        <div className={styles.sep}>
+<Link to="/services"className={styles.nt} >Services</Link>
+<Link to="/support" className={styles.nt}>Support</Link>
+ <Link to="/login"className={styles.nt}>Login</Link>  
+        </div>  
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
+
 // import { Link } from 'react-router-dom';
 // import React from "react";
 // import CoexLogo from '../resources/coex-logo (1).png';
@@ -11,38 +38,12 @@
 //         </div>
 //         <div className={style.rightnav}>
 
-//     <Link to="/services" className={style.nt}>Services</Link>
-//     <Link to="/support" className={style.nt}>Support</Link>
-//     <Link to="/login" className={style.nt}>Login</Link>
+{/* <Link to="/services" className={style.nt}>Services</Link>
+<Link to="/support" className={style.nt}>Support</Link>
+// <Link to="/login" className={style.nt}>Login</Link> */}
 
 //         </div>
 //     </div>
 //     }
 
 // export default NavBar;
-import React from "react";
-import CoexLogo from "../resources/coex-logo (1).png";
-import styles from "./style.module.css";
-
-function NavBar() {
-  return (
-    <div className={styles.NavbarContainer}>
-      <div className={styles.LeftNav}>
-        <img id="coexlogo" src={CoexLogo} alt="Logo" />
-      </div>
-
-      <div className={styles.RightNav}>
-        <div className={styles.sep}>
-          <h4>Services</h4>
-          <h4>About</h4>
-          <h4>Support</h4>
-        </div>  
-
-        <h4>Login</h4>
-      </div>
-    </div>
-  );
-}
-
-export default NavBar;
-
